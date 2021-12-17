4712b5d7ccaf0fb9bb5d5c7dcc1fae47ed3dbf96
gerard.queralt <gerard.queralt@estudiantat.upc.edu>
2021-12-17 20:51:28 +0100

    Day 5 Part 2
    I should've read the problem before choosing the language

--- src/day5/HydrothermalVenture.hs
+++ src/day5/HydrothermalVenture.hs
@@ -33,19 +33,46 @@ extractCoords coord = [(x1, y1), (x2, y2)]
         [x2, y2] = map (\s -> read s :: Integer) $ splitOn "," coord2
         
 insertCoords :: Integer -> Integer -> Integer -> Integer -> [[String]] -> [[String]]
-insertCoords x1 y1 x2 y2 hydroMap = populateMap 0 0 hydroMap
+insertCoords x1 y1 x2 y2 hydroMap = if x1 == x2 || y1 == y2 then populateHorVer else populateDia
     where
-        populateMap _ _ [] = []
-        populateMap curX curY (row:curMap) = (populateRow curX curY row):(populateMap curX (curY + 1) curMap)
-        populateRow _ _ [] = []
-        populateRow curX curY (column:row) = (populateColumn curX curY column):(populateRow (curX + 1) curY row)
-        populateColumn _ _ [] = []
-        populateColumn curX curY column
-            | x1 == x2 || y1 == y2 = if (min x1 x2) <= curX && curX <= (max x1 x2) && 
-                                        (min y1 y2) <= curY && curY <= (max y1 y2)
-                                        then increment column
-                                        else column
-            | otherwise = column -- tmp, for part 1
+        populateHorVer = populateMapHorVer 0 0 hydroMap
+            where
+                ((minX, minY),(maxX, maxY)) = ((min x1 x2, min y1 y2),(max x1 x2, max y1 y2))
+                populateMapHorVer _ _ [] = []
+                populateMapHorVer curX curY (row:curMap)
+                    | curY > maxY = (row:curMap)
+                    | otherwise = (populateRow curX curY row):(populateMapHorVer curX (curY + 1) curMap)
+                populateRow _ _ [] = []
+                populateRow curX curY (column:row)
+                    | curX > maxX = (column:row)
+                    | otherwise = (populateColumn curX curY column):(populateRow (curX + 1) curY row)
+                populateColumn _ _ [] = []
+                populateColumn curX curY column = if minX <= curX && curX <= maxX && 
+                                                    minY <= curY && curY <= maxY
+                                                    then increment column
+                                                    else column
+        
+        populateDia = populateMapDia 0 0 (minX, minY) hydroMap
+            where
+                ((minX, minY),(maxX, maxY)) = if y1 < y2 then ((x1, y1),(x2, y2)) else ((x2, y2),(x1, y1))
+                incOrDecX = if minX < maxX then (\x -> x + 1) else (\x -> x - 1)
+                populateMapDia _ _ _ [] = []
+                populateMapDia curX curY (targetX, targetY) (row:curMap)
+                    | curY > maxY = (row:curMap)
+                    | otherwise = if newRow == row
+                                     then row:(populateMapDia curX (curY + 1) (targetX, targetY) curMap)
+                                     else newRow:(populateMapDia curX (curY + 1) (incOrDecX targetX, targetY + 1) curMap)
+                                     where newRow = (populateRow curX curY (targetX, targetY) row)
+                populateRow _ _ _ [] = []
+                populateRow curX curY (targetX, targetY) (column:row)
+                    | curX > targetX = (column:row)
+                    | otherwise = (populateColumn curX curY (targetX, targetY) column):(populateRow (curX + 1) curY (targetX, targetY) row)
+                populateColumn _ _ _ [] = []
+                populateColumn curX curY (targetX, targetY) column = if curX == targetX &&
+                                                                        curY == targetY
+                                                                        then increment column
+                                                                        else column
+        
         increment column
             | column == "." = "1"
             | otherwise = show((read column :: Integer) + 1)
