36fe8beb76f9dbdde62b7639f72f2d740264b2bf
gerard.queralt <gerard.queralt@estudiantat.upc.edu>
2021-12-17 03:52:39 +0100

    Day 5 works but requires initialization

--- src/day5/HydrothermalVenture.hs
+++ src/day5/HydrothermalVenture.hs
@@ -15,36 +15,24 @@ extractCoords coord = [(x1, y1), (x2, y2)]
         [x2, y2] = map (\s -> read s :: Integer) $ splitOn "," coord2
         
 insertCoords :: Integer -> Integer -> Integer -> Integer -> [[String]] -> [[String]]
-insertCoords x1 y1 x2 y2 hydroMap = populate 0 0 hydroMap
+insertCoords x1 y1 x2 y2 hydroMap = populateMap 0 0 hydroMap
     where
-        populate curX curY (row:curMap)
-            | curX == x2 && curY == y2 = ((incrementCount (row:curMap)):(tail row)):curMap
-            | curX >= x1 && curY >= y1 = ((incrementCount (row:curMap)):(tail row)):next
-            | otherwise = next
-                where
-                    next = populate newX newY $ advance curX curY (row:curMap)
-                    newX = if curX == x2 then 0 else curX + 1
-                    newY = if newX == 0 then curY + 1 else curY
-        populate curX curY curMap
-            | curX == x2 && curY == y2 = ((incrementCount curMap):[]):curMap
-            | otherwise = curMap
-        
-        incrementCount ([]) = "1" --tmp, just so it compiles
-        incrementCount ([]:curMap) = "1"
-        incrementCount (([]:row):curMap) = "1"
-        incrementCount ((column:row):curMap)
-            | column == "." = "1"
-            | otherwise = show (1 + read(column)::Integer)
-        
-        advance curX curY (row:curMap) = if curX == x2 then curMap else if null row then ([]:curMap) else ((tail row):curMap)
+        populateMap _ _ [] = []
+        populateMap curX curY (row:curMap) = (populateRow curX curY row):(populateMap curX (curY + 1) curMap)
+        populateRow _ _ [] = []
+        populateRow curX curY (column:row) = (populateColumn curX curY column):(populateRow (curX + 1) curY row)
+        populateColumn _ _ [] = []
+        populateColumn curX curY column = if x1 <= curX && curX <= x2 && 
+                                             y1 <= curY && curY <= y2
+                                             then "1"
+                                             else column
 
 main :: IO ()
 main = do
     inputFile <- readFile "input.txt"
     let inputLines = lines inputFile
-    let tmp = [[".","."]]
-    let hydroMap = drawHydroMap inputLines tmp
-    mapM_ printRow tmp
+    let initializedMap = [[".","."],[".","."]]
+    let hydroMap = drawHydroMap inputLines initializedMap
     mapM_ printRow hydroMap
 
 printRow :: [String] -> IO ()
