470bd204b2889c8cf683962e2ffe882e04fe7340
gerard.queralt <gerard.queralt@estudiantat.upc.edu>
2021-12-17 02:10:52 +0100

    Day 5 WIP
    this would be so much easier in any other language but i'm to stubborn to change now

---
+++ src/day5/HydrothermalVenture.hs
@@ -0,0 +1,45 @@
+import Data.List.Split --sudo apt-get install libghc-split-dev
+
+drawHydroMap :: [String] -> [[String]] -> [[String]]
+drawHydroMap [] hydroMap = hydroMap
+drawHydroMap (coord:cl) hydroMap = drawHydroMap cl updatedMap
+    where
+        updatedMap = insertCoords x1 y1 x2 y2 hydroMap
+        [(x1,y1),(x2,y2)] = extractCoords coord
+        
+extractCoords :: String -> [(Integer, Integer)]
+extractCoords coord = [(x1, y1), (x2, y2)]
+    where
+        [coord1, coord2] = splitOn " -> " coord
+        [x1, y1] = map (\s -> read s :: Integer) $ splitOn "," coord1
+        [x2, y2] = map (\s -> read s :: Integer) $ splitOn "," coord2
+        
+insertCoords :: Integer -> Integer -> Integer -> Integer -> [[String]] -> [[String]]
+insertCoords x1 y1 x2 y2 hydroMap = populate 0 0 hydroMap
+    where
+        populate curX curY (row:curMap)
+            | curX == x2 && curY == y2 = curMap
+            | curX == x1 && curY == y2 = ((incrementCount (row:curMap)):row):next
+            | otherwise = (".":row):next
+                where
+                    next = populate newX newY $ advance curX curY (row:curMap)
+                    newX = if curX == x2 then 0 else curX + 1
+                    newY = if newX == 0 then curY + 1 else curY
+        incrementCount ((column:row):curMap)
+            | column == "." = "1"
+            | otherwise = show (1 + read(column)::Integer)
+        advance curX curY (row:curMap) = if curX == x2 then curMap else ((tail row):curMap)
+
+main :: IO ()
+main = do
+    inputFile <- readFile "input.txt"
+    let inputLines = lines inputFile
+    let hydroMap = drawHydroMap inputLines [[]]
+    mapM_ printRow hydroMap
+
+printRow :: [String] -> IO ()
+printRow [] = do
+    putStrLn ""
+printRow (l:ls) = do
+    putStr l
+    printRow ls
